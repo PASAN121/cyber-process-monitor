44019d93eeae91d5acf1a032230b82508d96818d
Pasanchandima123 <PASANCHANDI@GMAIL.COM>
2026-05-08 20:11:35 +0530

    correcting logics in monitor.py

--- src/monitor.py
+++ src/monitor.py
@@ -1,5 +1,11 @@
-import psutil
+import psutil #to monitor the precessor and memory status of the computer
 
+TotalMemory=psutil.virtual_memory().total
+NumCores=psutil.cpu_count() #get the number of cores 
+
+
+
+#Get process data  
 def get_processes():
     """Get running processes and store them as a list of dictionaries"""
     
@@ -9,9 +15,27 @@ def get_processes():
         try:
             info = p.info
 
-            mem_mb = (info["memory_info"].rss / 1024 / 1024) if info["memory_info"] else 0.0
-            cpu = info["cpu_percent"] or 0.0
-            name = info["name"] or "unknown"
+             # Memory in MB
+            mem_bytes = info["memory_info"].rss if info["memory_info"] else 0
+            mem_mb = mem_bytes / (1024 * 1024)
+
+
+            #memory status
+
+            mem_status=get_status(mem_bytes/TotalMemory)
+
+
+            # Normalize CPU: Divide by number of cores so max is 100%
+            cpu = (info["cpu_percent"] or 0.0)/NumCores
+
+            #CPU status
+
+            cpu_status=
+
+            
+
+            name=info["name"] or "unknown"
+
 
             procs.append({
                 "pid": info["pid"],
@@ -20,11 +44,21 @@ def get_processes():
                 "mem": mem_mb,
             })
 
+            
+
         except (psutil.AccessDenied, psutil.NoSuchProcess):
             pass  # skip protected or vanished processes
 
     return procs
 
-total_ram = psutil.virtual_memory().total / (1024**3)
-cpu_cores = psutil.cpu_count(logical=True)
+
+def get_status(current_val,total_val):
+    percentage=(current_val/total_val) * 100
+
+    if percentage > 75:
+        return "ALERT"
+    elif percentage >50:
+        return "WARN"
+    else:
+        return "OK"
 
